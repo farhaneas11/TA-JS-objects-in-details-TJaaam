5431f3c0cef51b74643dcd69929beaa87f29563e
farhaneas11 <farhan.salam11@gmail.com>
2021-05-20 02:20:24 +0530

    adv js -oop - 1

---
+++ block-BJaahs/code/index.js
@@ -0,0 +1,24 @@
+function createData(title,options,index=0){
+    let data = {};
+    data.title = title;
+    data.options= options;
+    data.correctAnswerIndex = index;
+    data.isAnswerCorrect = function (index = 0){
+        if(this.correctAnswerIndex === index){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    data.getCorrectAnswer = function (index = 0){
+        return this.correctAnswerIndex;
+    }
+    return data;
+}
+console.log(createData.data);
+let user2 = createData.data({
+    title: 'Where is the capital of Jordan',
+    options: ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
+    correctAnswerIndex: 1,
+  });
